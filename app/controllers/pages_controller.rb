a0a643c8e5dc57a21269bbf44426429030aadf9a
Patrick Koers <pkoers75@gmail.com>
2023-03-04 17:12:48 +0100

    added function to send a notification to slack channel

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,24 @@ class PagesController < ApplicationController
   def home
     # command to send emails via mailtrap
     # TestMailer.test_email.deliver_now
+  end
 
+  private
 
+  def slack
+    # function to send a notification to Slack
+    url = 'https://hooks.slack.com/workflows/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'
+    payload = { Notification: 'A lot of noise from AMP' }
+
+    uri = URI(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true if uri.scheme == 'https'
+
+    request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+    request.body = payload.to_json
+
+    response = http.request(request)
+
+    puts response.body
   end
 end
