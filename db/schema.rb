4a7e09b8bcde2ee2df70aa1514e28a2bcf16cf53
Patrick Koers <pkoers75@gmail.com>
2023-03-04 21:03:04 +0100

    Entries model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_161722) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_200034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,9 +18,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_161722) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
